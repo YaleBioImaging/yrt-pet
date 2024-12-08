dada8f72b5edff318263036ef39ed86c29281416
Yassir Najmaoui <yssrnjm@gmail.com>
2024-12-08 15:16:04 -0500

    Add randomness to image parameters and data in unit test

--- yrt-pet/unit_tests/recon/test_Image.cpp
+++ yrt-pet/unit_tests/recon/test_Image.cpp
@@ -8,6 +8,9 @@
 
 #include "datastruct/image/Image.hpp"
 
+#include <ctime>
+#include <random>
+
 void checkTwoImages(const Image& img1, const Image& img2)
 {
 	const ImageParams& params1 = img1.getParams();
@@ -25,26 +28,53 @@ void checkTwoImages(const Image& img1, const Image& img2)
 
 TEST_CASE("image-readwrite", "[image]")
 {
-	ImageParams params1{50, 50, 50, 50, 50, 50};
+	std::default_random_engine engine(
+	    static_cast<unsigned int>(std::time(nullptr)));
+
+	std::uniform_int_distribution<int> imageSizeDistribution(25, 75);
+	std::uniform_real_distribution<float> imageLengthDistribution(25.0f, 75.0f);
+	std::uniform_real_distribution<float> imageDataDistribution(0.0f, 1.0f);
+
+	std::string tmpImage_fname = "tmp.nii";
+	std::string tmpParams_fname = "tmp_params.json";
+
+	int nx = imageSizeDistribution(engine);
+	int ny = imageSizeDistribution(engine);
+	int nz = imageSizeDistribution(engine);
+	float length_x = imageLengthDistribution(engine);
+	float length_y = imageLengthDistribution(engine);
+	float length_z = imageLengthDistribution(engine);
+
+	ImageParams params1{nx, ny, nz, length_x, length_y, length_z};
 	ImageOwned img1{params1};
 	img1.allocate();
-	img1.writeToFile("tmp.nii");
 
-	ImageOwned img2{"tmp.nii"};
+	// Fill the image with random values
+	float* imgData_ptr = img1.getRawPointer();
+	int numVoxels = nx*ny*nz;
+	for(int i=0;i<numVoxels;i++)
+	{
+		imgData_ptr[i] = imageDataDistribution(engine);
+	}
+
+	img1.writeToFile(tmpImage_fname);
+
+	ImageOwned img2{tmpImage_fname};
 	ImageParams params2 = img2.getParams();
 	REQUIRE(params2.isSameAs(params1));
 
 	checkTwoImages(img1, img2);
 
-	params1.serialize("tmp_params.json");
-	ImageParams params3{"tmp_params.json"};
+	params1.serialize(tmpParams_fname);
+	ImageParams params3{tmpParams_fname};
 	REQUIRE(params1.isSameAs(params3));
 
-	ImageOwned img3{params3, "tmp.nii"};
+	ImageOwned img3{params3, tmpImage_fname};
 	REQUIRE(params1.isSameAs(img3.getParams()));
 
 	checkTwoImages(img1, img3);
 
-	std::remove("tmp.nii");
-	std::remove("tmp_params.json");
+	// Clear temporary files from disk
+	std::remove(tmpImage_fname.c_str());
+	std::remove(tmpParams_fname.c_str());
 }

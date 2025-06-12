ea20ffc5d4dab1781649e401d880043ebc39e8db
Yassir Najmaoui <yssrnjm@gmail.com>
2025-06-12 12:56:19 -0400

    Add debugging prints

--- yrt-pet/src/recon/Corrector_GPU.cu
+++ yrt-pet/src/recon/Corrector_GPU.cu
@@ -81,6 +81,12 @@ void Corrector_GPU::precomputeAdditiveCorrectionFactors(
 			acf = additiveCorrectionsPtr[bin];
 		}
 
+		// TODO NOW: This debug, remove this
+		std::cout << "Scatter: " << scatterEstimate << "\n";
+		std::cout << "Randoms: " << randomsEstimate << "\n";
+		std::cout << "Sensitivity: " << sensitivity << "\n";
+		std::cout << "ACF: " << acf << "\n";
+
 		if (acf > StabilityEpsilon && sensitivity > StabilityEpsilon)
 		{
 			additiveCorrectionsPtr[bin] =

1af17932df765ed9df779140caaa2e6177a77d25
Yassir Najmaoui <43155068+yassirnajmaoui@users.noreply.github.com>
2025-06-30 19:09:19 -0400

    Add simple timing (#71)
    * Add timer class
    
    * Add time usage and fix structure in the class
    
    * print timing statistics regardless of when the program exists

---
+++ yrt-pet/include/utils/Timer.hpp
@@ -0,0 +1,35 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
+namespace Util
+{
+	class Timer
+	{
+		using walltime_t = std::chrono::time_point<std::chrono::system_clock>;
+
+		using duration_t =
+		    std::chrono::duration<signed long int, std::ratio<1, 1000000000>>;
+
+	public:
+		Timer();
+
+		void reset();
+		void run();
+		void pause();
+
+		duration_t getElapsedTime() const;
+		double getElapsedMilliseconds() const;
+		bool isRunning() const;
+
+	private:
+		bool m_running;
+		duration_t m_elapsedTime;
+		walltime_t m_latestTime;
+	};
+}  // namespace Util

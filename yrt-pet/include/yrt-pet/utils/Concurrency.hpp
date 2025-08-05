239e045554bf5b4550b9143d36082f1e9a7b58e2
Yassir Najmaoui <yssrnjm@gmail.com>
2025-08-04 23:36:57 -0400

    Add parallel for chunked

--- yrt-pet/include/yrt-pet/utils/Concurrency.hpp
+++ yrt-pet/include/yrt-pet/utils/Concurrency.hpp
@@ -8,21 +8,56 @@
 #include <thread>
 #include <vector>
 
-namespace yrt::util {
-  template<typename F, typename... Args>
-  void parallel_do (size_t threadCnt, F f, Args... args) {
-    std::vector<std::thread> workers;
-    workers.reserve(threadCnt);
-    for (size_t i = 0; i < threadCnt; i++) workers.emplace_back(f, args...);
-    for (auto& worker : workers) worker.join();
-  }
+namespace yrt::util
+{
 
-  template<typename F, typename... Args>
-  void parallel_do_indexed (size_t threadCnt, F f, Args... args) {
-    std::vector<std::thread> workers;
-    workers.reserve(threadCnt);
-    for (size_t i = 0; i < threadCnt; i++) workers.emplace_back(f, i, args...);
-    for (auto& worker : workers) worker.join();
-  }
+template <typename F, typename... Args>
+void parallel_do(size_t threadCnt, F f, Args... args)
+{
+	std::vector<std::thread> workers;
+	workers.reserve(threadCnt);
+	for (size_t i = 0; i < threadCnt; i++)
+		workers.emplace_back(f, args...);
+	for (auto& worker : workers)
+		worker.join();
 }
 
+template <typename F, typename... Args>
+void parallel_do_indexed(size_t threadCnt, F f, Args... args)
+{
+	std::vector<std::thread> workers;
+	workers.reserve(threadCnt);
+	for (size_t i = 0; i < threadCnt; i++)
+		workers.emplace_back(f, i, args...);
+	for (auto& worker : workers)
+		worker.join();
+}
+
+template <typename Func>
+void parallel_for_chunked(size_t total, size_t threadCnt, Func fn)
+{
+	const size_t chunk = total / threadCnt;
+	std::vector<std::thread> threads;
+	threads.reserve(threadCnt);
+
+	for (unsigned t = 0; t < threadCnt; ++t)
+	{
+		const size_t start = t * chunk;
+		const size_t end = (t + 1 == threadCnt) ? total : start + chunk;
+		threads.emplace_back(
+		    [=, &fn]()
+		    {
+			    for (size_t i = start; i < end; ++i)
+			    {
+				    fn(i);
+			    }
+		    });
+	}
+
+	for (auto& th : threads)
+	{
+		th.join();
+	}
+}
+
+}  // namespace yrt::util

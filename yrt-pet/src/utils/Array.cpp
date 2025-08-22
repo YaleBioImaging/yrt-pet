dbad34f58f96a2bccdfccf954d47ef36c5b9db84
Yassir Najmaoui <yssrnjm@gmail.com>
2025-08-22 17:06:30 -0400

    Add note on Array.cpp

--- yrt-pet/src/utils/Array.cpp
+++ yrt-pet/src/utils/Array.cpp
@@ -59,6 +59,8 @@ void declare_array(pybind11::module& m, std::string type_str)
 
 void py_setup_array(pybind11::module& m)
 {
+	// TODO: Add Python bindings for the "Alias" type
+
 	// Add common array types
 	PY_DECLARE_ARRAY(float, 1);
 	PY_DECLARE_ARRAY(float, 2);

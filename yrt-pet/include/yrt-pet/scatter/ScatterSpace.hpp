db071011a92f10db3cb89a5197e8034b70030270
Yassir Najmaoui <yssrnjm@gmail.com>
2025-12-04 19:05:11 -0500

    Add some support for TOF

--- yrt-pet/include/yrt-pet/scatter/ScatterSpace.hpp
+++ yrt-pet/include/yrt-pet/scatter/ScatterSpace.hpp
@@ -10,15 +10,52 @@
 namespace yrt
 {
 /*
- * This class defines an undersampled space where to store values in an array
- * This assumes that the given scanner is centered in (0,0,0)
+ * This class defines an undersampled space where values are stored in a 5D
+ * array.
+ * This assumes that the given scanner is centered in (0,0,0).
+ * The scatter space assumes virtual "detectors" uniformly distributed in a
+ * perfect cylinder. These virtual detectors form the endpoints of the LORs
+ * sampled by this space.
+ * The objective is to use this space to compute and store scatter estimates in
+ * this undersampled collection of LORs rather than a fully-sampled Histogram3D.
+ *
+ * The array structures the lines of responses using a pair of cylindrical
+ * coordinates (with constant radius) and a TOF value.
+ *
+ * The continuous indices are as follows:
+ *  - TOF value: Difference of arrival time between virtual detector 2 (t2) and
+ *    virtual detector 1 (t1), or "t2 - t1", in ps
+ *    - The TOF values are always positive and go from 0 to
+ *      "scanner diameter"/"speed of light"
+ *  - Plane position of virtual detector 1
+ *    - Goes from "-axialFOV/2" to "axialFOV/2"
+ *  - Angular position of virtual detector 1
+ *    - Goes from 0 to 2pi
+ *  - Plane position of virtual detector 2
+ *    - Same interval as for virtual detector 1
+ *  - Angular position of virtual detector 2
+ *    - Same interval as for virtual detector 1
+ *
+ * The logical indices of this array sample the continuous indices in a regular
+ * grid, similar to image voxels.
+ *
+ * Note that, for cases without TOF, this space would have duplicates, since an
+ * LOR representing d2->d1 would be equivalent to an LOR representing d1->d2.
+ *
+ * Also, this space is not designed to only hold scatter estimate values, it can
+ * be used to store any collection of float32 values.
  */
 class ScatterSpace : public Histogram
 {
 public:
 	struct ScatterSpaceIndex
 	{
+		size_t tofBin, planeIndex1, angleIndex1, planeIndex2, angleIndex2;
+	};
 
+	struct ScatterSpacePosition
+	{
+		float tof_ps, planePosition1, angle1, planePosition2, angle2;
 	};
 
 	// Read a scatter-space file to create this object
@@ -27,26 +64,37 @@ public:
 	ScatterSpace(const Scanner& pr_scanner, size_t p_numTOFBins,
 	             size_t p_numPlanes, size_t p_numAngles);
 
+	// IO
 	void readFromFile(const std::string& fname);
 	void writeToFile(const std::string& fname) const;
 
+	// Do nearest neighbor
+	ScatterSpaceIndex nearestNeighbor(const ScatterSpacePosition& pos) const;
+
 	float getAxialFOV() const;
+	float getRadius() const;
+	float getDiameter() const;
+	float getMaxTOF_ps() const;
 	size_t getNumPlanes() const;
 	size_t getNumAngles() const;
-	float getRadius() const;
 
-	float getTOF_mm(size_t TOFBin) const;
+	float getTOF_ps(size_t TOFBin) const;
 	float getPlanePosition(size_t planeIndex) const;
 	float getAngle(size_t angleIndex) const;  // In radians
 
-	float getTOFBinStep_mm() const;
+	float getTOFBinStep_ps() const;
 	float getAngleStep() const;
 	float getPlaneStep() const;
+
+	float getValue(const ScatterSpaceIndex& idx) const;
+	float getValue(size_t tofBin, size_t planeIndex1, size_t angleIndex1,
+	               size_t planeIndex2, size_t angleIndex2) const;
 
 	void symmetrize();
 
 	static float wrapAngle(float angle);
 	float clampPlanePosition(float planePosition) const;
+	float clampTOF(float tof_ps) const;
 
 private:
 	void initStepSizes();
@@ -55,10 +103,16 @@ private:
 	size_t m_numPlanes;
 	size_t m_numAngles;
 
-	float m_TOFBinStep_mm;
+	float m_TOFBinStep_ps;
 	float m_angleStep;
 	float m_planeStep;
 
+	// Dimensions:
+	//  - TOF bin
+	//  - plane index 1
+	//  - angle index 1
+	//  - plane index 2
+	//  - angle index 2
 	std::unique_ptr<Array5D<float>> mp_values;
 };
 

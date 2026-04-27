6904be359d877ed4aea5a3d5942d64353a0d9597
Yassir Najmaoui <yssrnjm@gmail.com>
2026-04-27 15:03:39 -0400

    Fixes for test_UpdateEM.cpp

--- yrt-pet/unit_tests/recon/test_UpdateEM.cpp
+++ yrt-pet/unit_tests/recon/test_UpdateEM.cpp
@@ -35,7 +35,6 @@ TEST_CASE("updateem-static", "[updateem]")
 		const float length_z = imageLengthDistribution(engine);
 
 		yrt::ImageParams params(nx, ny, nz, length_x, length_y, length_z);
-		const int numVoxels = nx * ny * nz;
 
 		auto mainImage =
 		    yrt::util::test::makeImageWithRandomPrism(params, &engine);
@@ -44,13 +43,12 @@ TEST_CASE("updateem-static", "[updateem]")
 		auto sensImage =
 		    yrt::util::test::makeImageWithRandomPrism(params, &engine);
 
-		auto mainImage_gpu = std::make_unique<yrt::ImageOwned>(mainImage->getParams());
-
-		mainImage_gpu->allocate();
-
-		auto mainImageDevice = std::make_unique<yrt::ImageDeviceOwned>(mainImage.get());
-		auto updateImageDevice = std::make_unique<yrt::ImageDeviceOwned>(updateImage.get());
-		auto sensImageDevice = std::make_unique<yrt::ImageDeviceOwned>(sensImage.get());
+		auto mainImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(mainImage.get());
+		auto updateImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(updateImage.get());
+		auto sensImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(sensImage.get());
 
 		const float threshold = thresholdDistribution(engine);
 
@@ -60,6 +58,9 @@ TEST_CASE("updateem-static", "[updateem]")
 		mainImageDevice->updateEMThresholdStaticDevice(
 		    updateImageDevice.get(), sensImageDevice.get(), threshold, true);
 
+		auto mainImage_gpu =
+		    std::make_unique<yrt::ImageOwned>(mainImage->getParams());
+		mainImage_gpu->allocate();
 		mainImageDevice->transferToHostMemory(mainImage_gpu.get());
 
 		REQUIRE(yrt::util::test::allclose(*mainImage, *mainImage_gpu, 0.001,
@@ -75,11 +76,10 @@ TEST_CASE("updateem-dynamic-with-scaling", "[updateem]")
 	std::uniform_int_distribution<int> numVoxelsDistribution(10, 30);
 	std::uniform_int_distribution<int> numFramesDistribution(3, 8);
 	std::uniform_real_distribution<float> imageLengthDistribution(10.0f, 20.0f);
-	std::uniform_real_distribution<float> imageDataDistribution(0.1f, 10.0f);
 	std::uniform_real_distribution<float> thresholdDistribution(0.01f, 0.5f);
 	std::uniform_real_distribution<float> scalingDistribution(0.5f, 2.0f);
 
-	constexpr int NumTrials = 3;
+	constexpr int NumTrials = 4;
 
 	for (int trial = 0; trial < NumTrials; trial++)
 	{
@@ -93,7 +93,6 @@ TEST_CASE("updateem-dynamic-with-scaling", "[updateem]")
 
 		yrt::ImageParams params(nx, ny, nz, length_x, length_y, length_z, 0.0f,
 		                        0.0f, 0.0f, nt);
-		const int numVoxelsPerFrame = nx * ny * nz;
 
 		auto mainImage =
 		    yrt::util::test::makeImageWithRandomPrism(params, &engine);
@@ -104,33 +103,48 @@ TEST_CASE("updateem-dynamic-with-scaling", "[updateem]")
 		auto sensImage =
 		    yrt::util::test::makeImageWithRandomPrism(sensParams, &engine);
 
-		auto mainImage_gpu = std::make_unique<yrt::ImageOwned>(mainImage->getParams());
-		auto updateImage_gpu = std::make_unique<yrt::ImageOwned>(updateImage->getParams());
-		auto sensImage_gpu = std::make_unique<yrt::ImageOwned>(sensImage->getParams());
-
-		mainImage_gpu->allocate();
-		updateImage_gpu->allocate();
-		sensImage_gpu->allocate();
-
-		auto mainImageDevice = std::make_unique<yrt::ImageDevice>(mainImage.get(), nullptr);
-		auto updateImageDevice = std::make_unique<yrt::ImageDevice>(updateImage.get(), nullptr);
-		auto sensImageDevice = std::make_unique<yrt::ImageDevice>(sensImage.get(), nullptr);
+		auto mainImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(mainImage.get());
+		auto updateImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(updateImage.get());
+		auto sensImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(sensImage.get());
 
 		const float threshold = thresholdDistribution(engine);
 
-		std::vector<float> scaling(nt);
-		for (int t = 0; t < nt; t++)
+		// One trial with no scalings
+		if (trial == 0)
 		{
-			scaling[t] = scalingDistribution(engine);
+			mainImage->updateEMThresholdDynamic(updateImage.get(),
+			                                    sensImage.get(), threshold);
+
+			mainImageDevice->updateEMThresholdDynamicWithScaling(
+			    updateImageDevice.get(), sensImageDevice.get(), nullptr,
+			    threshold, true);
+		}
+		else
+		{
+			std::vector<float> scaling(nt);
+			for (int t = 0; t < nt; t++)
+			{
+				scaling[t] = scalingDistribution(engine);
+			}
+
+			yrt::DeviceArray<float> scalingDevice(scaling.size());
+			scalingDevice.copyFromHost(scaling.data(), scaling.size(),
+			                           {nullptr, true});
+
+			mainImage->updateEMThresholdDynamic(
+			    updateImage.get(), sensImage.get(), scaling, threshold);
+
+			mainImageDevice->updateEMThresholdDynamicWithScaling(
+			    updateImageDevice.get(), sensImageDevice.get(),
+			    scalingDevice.getDevicePointer(), threshold, true);
 		}
 
-		mainImage->updateEMThresholdDynamic(updateImage.get(), sensImage.get(),
-		                                    scaling, threshold);
-
-		mainImageDevice->updateEMThresholdDynamicWithScaling(
-		    updateImageDevice.get(), sensImageDevice.get(), scaling.data(),
-		    threshold, true);
-
+		auto mainImage_gpu =
+		    std::make_unique<yrt::ImageOwned>(mainImage->getParams());
+		mainImage_gpu->allocate();
 		mainImageDevice->transferToHostMemory(mainImage_gpu.get());
 
 		REQUIRE(yrt::util::test::allclose(*mainImage, *mainImage_gpu, 0.001,
@@ -161,11 +175,8 @@ TEST_CASE("updateem-dynamic-with-4d-sens", "[updateem]")
 		const float length_y = imageLengthDistribution(engine);
 		const float length_z = imageLengthDistribution(engine);
 
-		yrt::ImageParams params(nx, ny, nz, length_x, length_y, length_z,
-		                        yrt::util::test::DEFAULT_OFFSET,
-		                        yrt::util::test::DEFAULT_OFFSET,
-		                        yrt::util::test::DEFAULT_OFFSET, nt);
-		const int numVoxelsPerFrame = nx * ny * nz;
+		yrt::ImageParams params(nx, ny, nz, length_x, length_y, length_z, 0.0f,
+		                        0.0f, 0.0f, nt);
 
 		auto mainImage =
 		    yrt::util::test::makeImageWithRandomPrism(params, &engine);
@@ -174,25 +185,12 @@ TEST_CASE("updateem-dynamic-with-4d-sens", "[updateem]")
 		auto sensImage =
 		    yrt::util::test::makeImageWithRandomPrism(params, &engine);
 
-		auto mainImage_gpu = std::make_unique<yrt::ImageOwned>(*mainImage);
-		auto updateImage_gpu = std::make_unique<yrt::ImageOwned>(*updateImage);
-		auto sensImage_gpu = std::make_unique<yrt::ImageOwned>(*sensImage);
-
-		mainImage_gpu->allocate();
-		updateImage_gpu->allocate();
-		sensImage_gpu->allocate();
-
-		auto mainImageDevice = std::make_unique<yrt::ImageDevice>(params);
-		auto updateImageDevice = std::make_unique<yrt::ImageDevice>(params);
-		auto sensImageDevice = std::make_unique<yrt::ImageDevice>(params);
-
-		mainImageDevice->allocate();
-		updateImageDevice->allocate();
-		sensImageDevice->allocate();
-
-		mainImageDevice->transferFromHostMemory(mainImage.get());
-		updateImageDevice->transferFromHostMemory(updateImage.get());
-		sensImageDevice->transferFromHostMemory(sensImage.get());
+		auto mainImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(mainImage.get());
+		auto updateImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(updateImage.get());
+		auto sensImageDevice =
+		    std::make_unique<yrt::ImageDeviceOwned>(sensImage.get());
 
 		const float threshold = thresholdDistribution(engine);
 
@@ -202,6 +200,9 @@ TEST_CASE("updateem-dynamic-with-4d-sens", "[updateem]")
 		mainImageDevice->updateEMThresholdDynamicWith4DSens(
 		    updateImageDevice.get(), sensImageDevice.get(), threshold, true);
 
+		auto mainImage_gpu =
+		    std::make_unique<yrt::ImageOwned>(mainImage->getParams());
+		mainImage_gpu->allocate();
 		mainImageDevice->transferToHostMemory(mainImage_gpu.get());
 
 		REQUIRE(yrt::util::test::allclose(*mainImage, *mainImage_gpu, 0.001,

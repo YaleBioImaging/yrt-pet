663e733451238553596f8f101b6e9adcf89b1a4c
Yassir Najmaoui <yssrnjm@gmail.com>
2026-09-11 15:25:35 -0400

    Add another assertion for safety

--- yrt-pet/src/operators/OperatorPsf.cpp
+++ yrt-pet/src/operators/OperatorPsf.cpp
@@ -40,7 +40,8 @@ void py_setup_operatorpsf(py::module& m)
 
 	c.def_static(
 	    "createGaussianKernel1D",
-	    [](float sigma, float voxelSize) {
+	    [](float sigma, float voxelSize)
+	    {
 		    return OperatorPsf::createGaussianKernel1D(sigma, voxelSize,
 		                                               nullptr);
 	    },
@@ -169,11 +170,17 @@ std::vector<float> OperatorPsf::createGaussianKernel1D(float sigma,
 	{
 		constexpr size_t DEFAULT_NUM_SIGMAS = 5;
 		size = std::rintf(DEFAULT_NUM_SIGMAS * sigma / voxSize);
+		if (size % 2 == 0)
+		{
+			size++;
+		}
+		if (size < 3)
+		{
+			size = 3;
+		}
 	}
-	if (size % 2 == 0)
-	{
-		size++;
-	}
+	ASSERT_MSG(size >= 3 && size % 2 == 1,
+	           "The given size must be an odd number larger than 3");
 
 	std::vector<float> kernel;
 	for (std::size_t i = 0; i < size; i++)
